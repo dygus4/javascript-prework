ab8a31edfe7414dab13da58a6da1f29f7cbb4d3b
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-03-09 19:08:18 +0100

    Add buttons

--- js/script.js
+++ js/script.js
@@ -63,3 +63,12 @@ clearMessages()
 
 
 }
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame('1');
+  });
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame('2');
+  });
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame('3');
+  });
