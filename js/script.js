c7034bea30412c272a936f62e951ebeafb0f9a05
Wojciech Dyga <wojtek.dyga2@wp.pl>
2021-03-09 17:18:42 +0100

    Remove /* */ in JavaScript

--- js/script.js
+++ js/script.js
@@ -17,17 +17,8 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = getMoveName(randomNumber);
 
-
-
-/*if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if(randomNumber == 2){
-    computerMove = 'papier';
-} else if(randomNumber == 3){
-  computerMove = 'nożyce';
-}*/
-
 printMessage('Mój ruch to: ' + computerMove);
+
 
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
@@ -36,15 +27,6 @@ console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = getMoveName(playerInput);
 
-/*
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if(playerInput == "2"){
-  playerMove = "papier";
-} else if(playerInput == "3"){
-  playerMove = "nożyce";
-}
-*/
 printMessage('Twój ruch to: ' + playerMove);
 
 
@@ -76,30 +58,3 @@ function displayResult(argComputerMove, argPlayerMove){
 displayResult (computerMove, playerMove);
 
 
-
-
-
-/*
-
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-} else if( computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis!');
-} else if( computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Ty przegrywasz!');
-} else if( computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis!');
-} else if( computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Ty przegrywasz!');
-} else if( computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
-} else if( computerMove == 'nożyce' && playerMove == 'papier'){
-    printMessage('Ty przegrywasz!');
-} else if( computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
-} else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('Remis!');
-} else if( playerMove == 'nieznany ruch'){
-    printMessage('Ty przegrywasz!');
-}
-*/
